5731d8cef0f45eb388007584e0b92753715439d6
rantositorus <rantositorus44@gmail.com>
2024-06-19 08:34:27 +0700

    Tugas Hari-7

--- components/header/index.js
+++ components/header/index.js
@@ -1,11 +1,23 @@
 import { withAuth } from "../with-auth";
 import Menu from "../menu";
+import Link from "next/link";
 import styles from "./styles.module.css";
 
 function Header() {
   return (
     <div className={styles.header}>
       <Menu />
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link href="/users">Users</Link>
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ pages/users/index.js
@@ -0,0 +1,9 @@
+import Layout from "@/layout";
+
+export default function Users() {
+  return (
+    <Layout>
+      <h1>Users Page</h1>
+    </Layout>
+  );
+}
