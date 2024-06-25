0ef34c8d59fe7da89eba1efb5820782a42edcbd1
rantositorus <rantositorus44@gmail.com>
2024-06-25 07:58:53 +0700

    Tugas Hari-9

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  // res.status(200).json({ name: "John Doe" });
+  try {
+    const respnse = await (await fetch("https://dummyjson.com/users")).json();
+    res.status(200).json({ ...respnse });
+  } catch (error) {
+    console.log("error => ", error);
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Main() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response => ", res))
+      .catch((err) => console.log("error => ", err));
+  }, []);
   return (
     <>
       <Layout metaTitle="Home" metaDescription="Ini adalah halaman beranda">
